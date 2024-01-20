7cb8db3aa079a62e4354e2a38da3f108a742050d
Neal Uehara <ueharaneal@gmail.com>
2024-01-20 03:45:41 -0800

    connected Server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
+
+const port = process.env.PORT || 3001
 
 const app = express()
-const PORT = 3001
 
-app.get('/',(req,res)=>{
-    res.send('home page')
+app.use(bodyParser.json())
+
+app.listen(port, ()=>{
+    console.log(`App listing on port ${port}`)
 })
-
-app.listen(PORT,()=>console.log(`The server is listening on port ${PORT}`))
