4a0ea86cdbc080d28f8de3d1549d12b3e7526e43
Neal Uehara <ueharaneal@gmail.com>
2024-01-21 03:48:24 -0800

    pushed code:

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 
 const port = process.env.PORT || 3001
 
 const app = express()
 
+app.use(cors())
+
 app.use(bodyParser.json())
+
+app.get('/api/users',(req,res,next)=>{
+    res.json([
+        {name: "neal", lastName: "uehara"},
+        {name: "john", lastName: "uehara"}
+    ])
+})
 
 app.listen(port, ()=>{
     console.log(`App listing on port ${port}`)
