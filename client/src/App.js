88cbd580ac1f3b1e56ca6865ca506ebe4a91285e
Neal Uehara <ueharaneal@gmail.com>
2024-01-21 03:57:49 -0800

    axios

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import { useEffect } from "react";
 import axios from "axios";
 
 function App() {
-    useEffect(() => {
-      axios
-        .get("/api/users")
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => console.error("Error fetching data", error));
-    }, []);
-    return <div> App</div>;
-  }
+  useEffect(() => {
+    axios
+      .get("api/users")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error, "this is the error");
+      });
+  }, []);
+  return <div> App</div>;
+}
 
 export default App;
